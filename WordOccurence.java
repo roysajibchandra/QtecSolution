9faaeff13ce1328dade8812921db29cf1b485857
Sajib Chandra Roy <93840340+roysajibchandra@users.noreply.github.com>
2024-03-09 22:16:53 +0600

    Committed by Sajib

--- WordOccurence.java
+++ WordOccurence.java
@@ -2,16 +2,23 @@ import java.util.*;
 
 public class WordOccurence {
   public static void main(String[] args) {
-    
+
     Scanner input = new Scanner(System.in);
     // String line = input.nextLine().toString();
     String line = input.nextLine().toLowerCase();
 
+    // TreeMap is declared to store words by tree order
+
     Map<String, Integer> map = new TreeMap<>();
+
+    // this looping for ignore punctuation marks, whitespace, and case sensitivity
+    // appropriately the sentence.
+
     for (String word : line.split("[\\W|\\s]+")) {
       map.put(word, map.getOrDefault(word, 0) + 1);
     }
 
+    // this looping for count the unique word for the sentence
     for (Map.Entry entry : map.entrySet()) {
       System.out.println(entry.getKey() + ": " + entry.getValue());
     }
